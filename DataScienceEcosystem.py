d8545820f42f7923a8ee27f27cf68d11aaa170f8
akhil5ca <140004118+akhil5ca@users.noreply.github.com>
2023-07-20 21:32:04 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# * Overview of Data Science Tools
+# * Languages of Data Science
+# * Jupiter Project
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Scala
+
+# Some of the commonly used libraries used by Data Scientists include:
+# * Matlibplot
+# * Pandas
+# * NumPy
+# * SciPy
+
+# | Data Science Tools |
+# |--------------------|
+# | IBM Watson Studio  |
+# |     Tableau        |
+# |   Apache Spark     |
+# |--------------------|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+print ((3*4)+5)
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+print(f"{200/60} Hours")
+
+
+# ## Author
+# Akhil
+
+# In[ ]:
+
+
+
+
